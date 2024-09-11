899f5aa07dc0a99214cacafb32057d902bfadd3f
Bastien Ricard <bastien.ricard-dev@outlook.com>
2024-09-11 15:24:00 +0200

    Add hero mobile style

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 function App() {
   return (
     <>
-      <main>
-        Reliable, efficient delivery Powered by Technology Our Artificial
-        Intelligence powered tools use millions of project data points to ensure
-        that your project is successful Supervisor Monitors activity to identify
-        project roadblocks Team Builder Scans our talent network to create the
-        optimal team for your project Karma Regularly evaluates our talent to
-        ensure quality Calculator Uses data from past projects to provide better
-        delivery estimates
+      <main className="mt-[5.3125rem] px-8">
+        <div className="space-y-4">
+          <h1 className="text-center text-2xl font-light leading-normal tracking-[.17px]">
+            Reliable, efficient delivery{' '}
+            <span className="block font-semibold">Powered by Technology</span>
+          </h1>
+          <p className="max-w-[540px] mx-auto text-center text-grayishBlue leading-[1.6] tracking-[.1px]">
+            Our Artificial Intelligence powered tools use millions of project
+            data points to ensure that your project is successful
+          </p>
+        </div>
+        Supervisor Monitors activity to identify project roadblocks Team Builder
+        Scans our talent network to create the optimal team for your project
+        Karma Regularly evaluates our talent to ensure quality Calculator Uses
+        data from past projects to provide better delivery estimates
       </main>
       <footer>
         <p className="text-center text-[0.6875rem]">
