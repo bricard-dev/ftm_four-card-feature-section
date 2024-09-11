6988be0ec3e06892122d55dab2326927d0dfebd3
Bastien Ricard <bastien.ricard-dev@outlook.com>
2024-09-11 13:01:17 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+function App() {
+  return (
+    <>
+      <main>
+        Reliable, efficient delivery Powered by Technology Our Artificial
+        Intelligence powered tools use millions of project data points to ensure
+        that your project is successful Supervisor Monitors activity to identify
+        project roadblocks Team Builder Scans our talent network to create the
+        optimal team for your project Karma Regularly evaluates our talent to
+        ensure quality Calculator Uses data from past projects to provide better
+        delivery estimates
+      </main>
+      <footer>
+        <p className="text-center text-[0.6875rem]">
+          Challenge by{' '}
+          <a
+            className="text-[#3e52a3]"
+            href="https://www.frontendmentor.io?ref=challenge"
+            target="_blank"
+          >
+            Frontend Mentor
+          </a>
+          . Coded by{' '}
+          <a className="text-[#3e52a3]" href="#">
+            Your Name Here
+          </a>
+          .
+        </p>
+      </footer>
+    </>
+  );
+}
+
+export default App;
