bb3889cb69e8291b8b4111383942c60b809cea50
Bastien Ricard <bastien.ricard-dev@outlook.com>
2024-09-12 16:51:56 +0200

    Add desktop design

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import Card from './components/Card';
 function App() {
   return (
     <>
-      <main className="mt-[5.3125rem] px-8 space-y-[76px]">
+      <main className="max-w-[1100px] w-full mt-[5.3125rem] xl:mt-20 mx-auto px-8 xl:p-0 space-y-[76px] xl:space-y-[64px]">
         <div className="space-y-4">
-          <h1 className="text-center text-2xl font-light leading-normal tracking-[.17px]">
+          <h1 className="text-center text-2xl xl:text-4xl font-light leading-normal xl:leading-[54px] tracking-[.17px]">
             Reliable, efficient delivery{' '}
             <span className="block font-semibold">Powered by Technology</span>
           </h1>
@@ -14,12 +14,13 @@ function App() {
             data points to ensure that your project is successful
           </p>
         </div>
-        <div className="grid gap-[25px]">
+        <div className="grid xl:grid-cols-3 xl:grid-rows-2 gap-[25px] xl:gap-[30px]">
           <Card
             title="Supervisor"
             description="Monitors activity to identify project roadblocks"
             imgSrc="./assets/icon-supervisor.svg"
             topColor="cyan"
+            className="xl:row-span-2 xl:self-center"
           />
           <Card
             title="Team Builder"
@@ -27,12 +28,14 @@ function App() {
         your project"
             imgSrc="./assets/icon-team-builder.svg"
             topColor="red"
+            className="xl:row-span-1"
           />
           <Card
             title="Karma"
             description="Regularly evaluates our talent to ensure quality"
             imgSrc="./assets/icon-karma.svg"
             topColor="orange"
+            className="xl:col-start-2 xl:row-start-2"
           />
           <Card
             title="Calculator"
@@ -40,6 +43,7 @@ function App() {
         estimates"
             imgSrc="./assets/icon-calculator.svg"
             topColor="blue"
+            className="xl:row-span-2 xl:self-center"
           />
         </div>
       </main>
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,28 +1,37 @@
-export default function Card({
-  title,
-  description,
-  imgSrc,
-  imgAlt,
-  topColor,
-}: {
+import { cn } from '../utils/cn';
+
+export interface CardProps {
   title: string;
   description: string;
   imgSrc: string;
   imgAlt?: string;
   topColor: string;
-}) {
+  className?: string;
+}
+
+export default function Card({
+  title,
+  description,
+  imgSrc,
+  imgAlt = `${title} icon`,
+  topColor = 'cyan',
+  className,
+}: CardProps) {
   return (
     <div
-      className={`p-7 relative flex flex-col rounded-lg shadow-card overflow-hidden before:w-full before:h-1 before:absolute before:top-0 before:left-0 before:bg-${topColor}`}
+      className={cn(
+        `xl:w-[350px] xl:h-[250px] p-7 relative flex flex-col rounded-lg shadow-card overflow-hidden before:w-full before:h-1 before:absolute before:top-0 before:left-0 before:bg-${topColor}`,
+        className
+      )}
     >
       <h2 className="font-semibold text-xl leading-10">{title}</h2>
-      <p className="mb-[33px] text-[.8125rem] text-grayishBlue leading-[1.7] tracking-[.09px]">
+      <p className="mb-[33px] xl:mb-10 text-[.8125rem] text-grayishBlue leading-[1.7] tracking-[.09px]">
         {description}
       </p>
       <img
-        className="w-[57px] h-[57px] self-end"
+        className="w-[57px] xl:w-[64px] h-[57px] xl:h-[64px] self-end"
         src={imgSrc}
-        alt={imgAlt || `${title} icon`}
+        alt={imgAlt}
       />
     </div>
   );
